4c9a426d9e0be3ec9378540bdca7864e36ee13bc
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-30 20:14:16 +0100

    Implement loadElections function
    Add loadElections function to display elections from localStorage

--- script.js
+++ script.js
@@ -29,3 +29,34 @@ function createElection() {
     window.location.href="dashboard.html";
 
 }
+function loadElections() {
+
+    const electionList = document.getElementById("electionList");
+
+    if (!electionList) return;
+
+    const elections =
+        JSON.parse(localStorage.getItem("elections")) || [];
+
+    if (elections.length === 0) {
+        electionList.innerHTML =
+            "<p>No elections created yet.</p>";
+        return;
+    }
+
+    electionList.innerHTML = "";
+
+    elections.forEach(function(election) {
+
+        electionList.innerHTML += `
+        <div class="card">
+            <h2>${election.name}</h2>
+            <p>${election.type}</p>
+        </div>
+        `;
+
+    });
+
+}
+
+loadElections();
